40a98898209224bdc542fd51682e38cfff34fd04
Leonardo Andriolo <159090875+LeonardoAndriolo@users.noreply.github.com>
2024-10-17 11:13:13 -0300

    feat: adiciona um gif de demonstração
    Também atualiza o blink.txt com informações do processo.

--- Blink.ino
+++ Blink.ino
@@ -22,13 +22,13 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
-// the setup function runs once when you press reset or power the board
+// the setup function (runs once when you press reset or power the board)
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+// the loop function (runs over and over again forever)
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   //delay(2000);                     // wait for 2000 ms
